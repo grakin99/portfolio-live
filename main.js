f8cab15386c0696b9538f4da8458b7e2c3bbf72c
beta <betatross@pm.me>
2025-11-08 19:51:49 -0700

    Additional check on JS event added.

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ const dialogTrigger = document.querySelectorAll('button.dialog');
 const dialogCloseButton = document.querySelectorAll('button.close-dialog');
 
 for (const trigger of dialogTrigger) {
-	trigger.addEventListener('click', function () {
+	trigger?.addEventListener('click', function () {
 		this?.nextElementSibling.showModal()
 	})
 }
 
 for (const button of dialogCloseButton) {
-	button.addEventListener('click', function () {
+	button?.addEventListener('click', function () {
 		this?.parentElement.close()
 	})
 }
